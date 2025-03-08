4b21dcc92a1e90786cad68af8dcab69d5bf4a67c
notoraptor <stevenbocco@gmail.com>
2025-03-08 09:58:52 -0500

    up

--- examples/prometheus.py
+++ examples/prometheus.py
@@ -2,9 +2,9 @@ from sarc.config import config
 
 
 def main():
-    for cluster in config().clusters:
+    for cluster in config().clusters.values():
         if cluster.prometheus_url:
-            print("PROMETHEUS")
+            print("PROMETHEUS", cluster.name)
             ret = cluster.prometheus.custom_query("slurm_job_utilization_gpu_memory")
             print(type(ret), len(ret))
             break
