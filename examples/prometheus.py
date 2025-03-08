23483768a713a8fcd64d52c69221b688108985a3
notoraptor <stevenbocco@gmail.com>
2025-03-08 10:34:33 -0500

    up

--- examples/prometheus.py
+++ examples/prometheus.py
@@ -3,9 +3,10 @@ from sarc.config import config
 
 def main():
     for cluster in config().clusters.values():
-        if cluster.prometheus_url and cluster.name == "beluga":
+        if cluster.prometheus_url and cluster.name != "mila":
             query = "slurm_job_utilization_gpu_memory"
             query = 'slurm_job_utilization_gpu_memory{slurmjobid=~"47622739"}[2371s:2371s]  offset 24929460s'
+            query = "slurm_job_utilization_gpu_memory[1m]"
             print("PROMETHEUS", cluster.name)
             ret = cluster.prometheus.custom_query(query)
             print(type(ret), len(ret))
